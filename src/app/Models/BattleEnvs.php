6df47ff97f3a06fd1019abbe6d62d94af76b9841
okina3 <takosos@icloud.com>
2024-03-21 22:32:45 +0900

    モデル、マイグレーションの作成

---
+++ src/app/Models/BattleEnvs.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BattleEnvs extends Model
+{
+    use HasFactory;
+}
---
+++ src/database/migrations/2024_03_21_223024_create_opp_selects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('opp_selects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('battle_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('pokemon_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('opp_selects');
+    }
+};
