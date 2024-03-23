820ffedf560fbe2b71990d5425ac6929abed1496
okina3 <takosos@icloud.com>
2024-03-23 20:34:58 +0900

    サービスへの切り離し

--- src/app/Http/Controllers/BattleController.php
+++ src/app/Http/Controllers/BattleController.php
@@ -4,12 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\BattleRequest;
 use App\Models\Battle;
-use App\Models\BattleEnvs;
 use App\Models\Envs;
-use App\Models\OppSelect;
-use App\Models\OppTeam;
-use App\Models\PlayerSelect;
 use App\Models\Pokemon;
+use App\Services\BattleService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -23,19 +20,8 @@ class BattleController extends Controller
      */
     public function index(): View
     {
-        // 全バトルデータ、または検索されたバトルデータを表示する
-        $get_url = \Request::query('envs');
-        // もしクエリパラメータがあれば、環境から絞り込む
-        if (!empty($get_url)) {
-            // 絞り込んだ環境にリレーションされたバトルデータを含む、環境を取得
-            $url_envs = Envs::with('battles')->availableSelectEnvs($get_url)->first();
-            // 環境にリレーションされたバトルデータを取得
-            $all_battle = $url_envs->battles;
-        } else {
-            // 全バトルデータを取得
-            $all_battle = Battle::availableAllBattle()->orderBy('created_at', 'desc')->get();
-        }
-
+        // 全バトルデータ、または検索されたバトルデータを一覧表示する
+        $all_battle = BattleService::searchBattles();
         // 全環境を取得
         $all_envs = Envs::availableAllEnvs()->get();
 
@@ -70,26 +56,8 @@ class BattleController extends Controller
             'judgment' => $request->judgment,
             'comment' => $request->comment,
         ]);
-
-        // 相手のチームをバトルデータに紐付けて中間テーブルに保存
-        foreach ($request->opp_teams as $opp_team) {
-            Battle::findOrFail($battle->id)->oppTeams()->attach($opp_team);
-        }
-
-        // 相手の選出をバトルデータに紐付けて中間テーブルに保存
-        foreach ($request->opp_selects as $opp_select) {
-            Battle::findOrFail($battle->id)->oppSelects()->attach($opp_select);
-        }
-
-        // 自分の選出をバトルデータに紐付けて中間テーブルに保存
-        foreach ($request->player_selects as $player_select) {
-            Battle::findOrFail($battle->id)->playerSelects()->attach($player_select);
-        }
-
-        // 環境をバトルデータに紐付けて中間テーブルに保存
-        foreach ($request->envs as $envs_number) {
-            Battle::findOrFail($battle->id)->envs()->attach($envs_number);
-        }
+        // バトルデータに紐づいた、相手のチーム、相手の選出、自分の選出、環境を、中間テーブルを保存するメソッド
+        BattleService::attachBattleRecords($request, $battle->id);
 
         return to_route('index')->with(['message' => 'バトルデータを登録しました。', 'status' => 'info']);
     }
@@ -120,30 +88,14 @@ class BattleController extends Controller
         $all_envs = Envs::availableAllEnvs()->get();
         // 選択したバトルデータを取得
         $select_battle = Battle::availableSelectBattle($id)->first();
-
-        // 選択したバトルデータに紐づいた相手のチームidを取得
-        $opp_teams = [];
-        foreach ($select_battle->oppTeams as $opp_team) {
-            $opp_teams[] = $opp_team->id;
-        }
-
-        // 選択したバトルデータに紐づいた相手の選出idを取得
-        $opp_selects = [];
-        foreach ($select_battle->oppSelects as $opp_select) {
-            $opp_selects[] = $opp_select->id;
-        }
-
-        // 選択したバトルデータに紐づいた自分の選出idを取得
-        $player_selects = [];
-        foreach ($select_battle->playerSelects as $player_select) {
-            $player_selects[] = $player_select->id;
-        }
-
+        // 選択したバトルデータに紐づいた、相手のチームidを取得
+        $opp_teams = BattleService::oppTeamId($select_battle);
+        // 選択したバトルデータに紐づいた、相手の選出idを取得
+        $opp_selects = BattleService::oppSelectId($select_battle);
+        // 選択したバトルデータに紐づいた、自分の選出idを取得
+        $player_selects = BattleService::playerSelectId($select_battle);
         // 選択したバトルデータに紐づいた環境idを取得
-        $envs = [];
-        foreach ($select_battle->envs as $env) {
-            $envs[] = $env->id;
-        }
+        $envs = BattleService::envsId($select_battle);
 
         return view(
             'battles.edit',
@@ -164,35 +116,10 @@ class BattleController extends Controller
         $battle->judgment = $request->judgment;
         $battle->comment = $request->comment;
         $battle->save();
-
-        // 一旦バトルデータと相手のチームを紐付けた中間デーブルのデータを削除
-        OppTeam::where('battle_id', $request->battleId)->delete();
-        // 一旦バトルデータと相手の選出を紐付けた中間デーブルのデータを削除
-        OppSelect::where('battle_id', $request->battleId)->delete();
-        // 一旦バトルデータと自分の選出を紐付けた中間デーブルのデータを削除
-        PlayerSelect::where('battle_id', $request->battleId)->delete();
-        // 一旦バトルデータと環境を紐付けた中間デーブルのデータを削除
-        BattleEnvs::where('battle_id', $request->battleId)->delete();
-
-        // 相手のチームをバトルデータに紐付けて中間テーブルに保存
-        foreach ($request->opp_teams as $opp_team) {
-            Battle::findOrFail($battle->id)->oppTeams()->attach($opp_team);
-        }
-
-        // 相手の選出をバトルデータに紐付けて中間テーブルに保存
-        foreach ($request->opp_selects as $opp_select) {
-            Battle::findOrFail($battle->id)->oppSelects()->attach($opp_select);
-        }
-
-        // 自分の選出をバトルデータに紐付けて中間テーブルに保存
-        foreach ($request->player_selects as $player_select) {
-            Battle::findOrFail($battle->id)->playerSelects()->attach($player_select);
-        }
-
-        // 環境をバトルデータに紐付けて中間テーブルに保存
-        foreach ($request->envs as $envs_number) {
-            Battle::findOrFail($battle->id)->envs()->attach($envs_number);
-        }
+        // バトルデータに、紐付いた中間デーブルのデータを全て削除
+        BattleService::relationDelete($request->battleId);
+        // バトルデータに紐づいた、相手のチーム、相手の選出、自分の選出、環境を、中間テーブルを保存するメソッド
+        BattleService::attachBattleRecords($request, $battle->id);
 
         return to_route('index')->with(['message' => 'バトルデータを更新しました。', 'status' => 'info']);
     }
